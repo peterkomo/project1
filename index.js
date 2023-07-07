4f1d93962848bf96d8c6a075c8712f7b4202631e
peterkomo <mwangipeterkomo@gmail.com>
2023-07-07 05:14:30 +0300

    get request the json file

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
    // Code to be executed when the DOM is fully loaded 
-})
+   fetch("http://localhost:3000/strong")
+   .then(function (response) {
+     return response.json();
+   })
+   //.then(function (data) {
+     // Get the 'strong' array from the JSON data
+     //let strongArray = data;
+
+     // Get the <ul> element with id 'strong'
+     //let strongList = document.getElementById("strong");
+ 
+   })
