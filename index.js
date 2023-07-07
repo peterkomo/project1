9d73bdeb8e94690424f664935f9d70f7f55d4993
peterkomo <mwangipeterkomo@gmail.com>
2023-07-07 05:28:43 +0300

    get the list element with the id strong

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
    // Code to be executed when the DOM is fully loaded 
    fetch("http://localhost:3000/strong")
-    .then(function (response) {
+    .then(function (response) {// Extract the JSON data from the response
      return response.json();
    })
    .then(function (data) {
+    // Handle the retrieved JSON data
      // Get the 'strong' array from the JSON data
      let strongArray = data;
 
      // Get the <ul> element with id 'strong'
-     //let strongList = document.getElementById("strong");
+      let strongList = document.getElementById("strong");
  
    })
